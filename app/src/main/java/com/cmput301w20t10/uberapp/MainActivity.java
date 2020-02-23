fcf63879e40158a1119e21fbbc2afc248ae73515
Allan Manuba <manuba@ualberta.ca>
2020-02-23 00:19:47 -0700

    Set up google maps api

--- app/src/main/java/com/cmput301w20t10/uberapp/MainActivity.java
+++ app/src/main/java/com/cmput301w20t10/uberapp/MainActivity.java
@@ -2,13 +2,22 @@ package com.cmput301w20t10.uberapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonDriverTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonDriverTest = findViewById(R.id.button_test_driver);
+        buttonDriverTest.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, DriverMainScreenActivity.class);
+            startActivity(intent);
+        });
     }
 }
