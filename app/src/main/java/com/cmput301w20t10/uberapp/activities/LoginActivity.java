fa5a8d29e8d72450738eed8040028df8a54b1afa
Sora <joshua90123@gmail.com>
2020-02-24 18:42:24 -0700

    Completed UI layout

---
+++ app/src/main/java/com/cmput301w20t10/uberapp/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.cmput301w20t10.uberapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cmput301w20t10.uberapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
