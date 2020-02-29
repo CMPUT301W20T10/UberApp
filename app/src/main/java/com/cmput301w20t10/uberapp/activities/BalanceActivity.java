59b85a1f618540e1498efdba46e04d70d6ef1d57
Kevin <kli7@ualberta.ca>
2020-02-28 17:50:01 -0700

    Balance Page

---
+++ app/src/main/java/com/cmput301w20t10/uberapp/activities/BalanceActivity.java
@@ -0,0 +1,18 @@
+package com.cmput301w20t10.uberapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cmput301w20t10.uberapp.R;
+
+public class BalanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.balance_page);
+
+    }
+
+}
--- app/src/main/java/com/cmput301w20t10/uberapp/activities/LoginActivity.java
+++ app/src/main/java/com/cmput301w20t10/uberapp/activities/LoginActivity.java
@@ -2,6 +2,7 @@ package com.cmput301w20t10.uberapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -31,12 +32,18 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLoginPressed(View view) {
-
+        Intent intent = new Intent(this, BalanceActivity.class);
+        startActivity(intent);
     }
 
     public void onRegisterPressed(View view) {
-
+        Intent intent = new Intent(this, BalanceActivity.class);
+        startActivity(intent);
     }
 
+    public void onBalancePressed(View view) {
+        Intent intent = new Intent(this, BalanceActivity.class);
+        startActivity(intent);
+    }
 
 }
