6c492d71bb0a47165acca7460c431e507b16919e
Sora <joshua90123@gmail.com>
2020-03-02 15:41:45 -0700

    Cleaned Up Register Screen Layout

--- app/src/main/java/com/cmput301w20t10/uberapp/activities/RegisterActivity.java
+++ app/src/main/java/com/cmput301w20t10/uberapp/activities/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.cmput301w20t10.uberapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.cmput301w20t10.uberapp.R;
 
@@ -13,4 +14,13 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    public void onCancelPress(View view) {
+
+    }
+
+    public void onRegisterPress(View view) {
+
+    }
+
 }
