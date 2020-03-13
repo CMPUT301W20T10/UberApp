01201237475d731e25b0ddc38b4f201df31416ca
alaevens <alaevens@ualberta.ca>
2020-03-13 13:12:00 -0600

    Merge in profile

--- app/src/main/java/com/cmput301w20t10/uberapp/activities/RiderMainActivity.java
+++ app/src/main/java/com/cmput301w20t10/uberapp/activities/RiderMainActivity.java
@@ -56,24 +56,7 @@ public class RiderMainActivity extends BaseActivity implements OnMapReadyCallbac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rider_main);
-        Toolbar toolbar = findViewById(R.id.toolbar_rider);
-        setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-
-        // on support navigate up
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        setContentView(R.layout.content_rider_main);
 
         // map
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
