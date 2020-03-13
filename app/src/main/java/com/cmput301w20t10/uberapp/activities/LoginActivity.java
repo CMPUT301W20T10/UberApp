b17ad1f01ae3a517b03ea6b97c875661a7cf3fe0
Kevin <kli7@ualberta.ca>
2020-03-13 15:00:16 -0600

    Bug Fixes

--- app/src/main/java/com/cmput301w20t10/uberapp/activities/LoginActivity.java
+++ app/src/main/java/com/cmput301w20t10/uberapp/activities/LoginActivity.java
@@ -2,6 +2,8 @@ package com.cmput301w20t10.uberapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
 import android.Manifest;
 import android.content.Intent;
@@ -17,90 +19,104 @@ import android.widget.Toast;
 
 import com.cmput301w20t10.uberapp.R;
 import com.cmput301w20t10.uberapp.database.DatabaseManager;
+import com.cmput301w20t10.uberapp.models.*;
+import com.cmput301w20t10.uberapp.database.LoginRegisterDAO;
+import com.cmput301w20t10.uberapp.database.RiderDAO;
+import com.cmput301w20t10.uberapp.models.Driver;
+import com.cmput301w20t10.uberapp.models.Rider;
 
 /**
  * @author Joshua Mayer
  * @version 1.0.2
  */
-public class LoginActivity extends AppCompatActivity {
+        public class LoginActivity extends AppCompatActivity {
 
-    private RadioButton radioButtonRider;
-    private Button buttonLogIn;
-    private EditText usernameField;
-    private EditText passwordField;
-    private RadioGroup loginTypeField;
-    private static final int REQUEST_CODE = 101;
+            private RadioButton radioButtonRider;
+            private Button buttonLogIn;
+            private EditText usernameField;
+            private EditText passwordField;
+            private RadioGroup loginTypeField;
+            private static final int REQUEST_CODE = 101;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_login);
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                }
+
+                radioButtonRider = findViewById(R.id.rider_radio_button);
+                buttonLogIn = findViewById(R.id.button_log_in);
+
+                buttonLogIn.setOnClickListener(view -> onLoginPressed());
+
+                this.usernameField = findViewById(R.id.username_field);
+                this.passwordField = findViewById(R.id.password_field);
+                this.loginTypeField = findViewById(R.id.rider_driver_toggle);
+
+                // Set the selector to select Rider by default
+                this.loginTypeField.check(R.id.rider_radio_button);
+            }
+
+            public void onLoginPressed() {
+                Log.d("Testing", "Verify Fields");
+                // Check for empty fields
+                if(usernameField.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Username Required", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if(passwordField.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Password Required", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                verifyLogin();
+            }
+
+            private void verifyLogin() {
+                // todo: proper implementation of sign in
+
+                if (radioButtonRider.isChecked()) {
+                    //login as rider
+                    Log.d("Testing", "Log in as rider!");
+                    MutableLiveData<Rider> liveRider = DatabaseManager.getInstance().logInAsRider(
+                            usernameField.getText().toString(), passwordField.getText().toString(), this);
+                    liveRider.observe(this, rider -> {
+                        if (rider != null) {
+                            Log.d("Testing", "Login Success");
+                            Intent intent = new Intent(this, RiderMainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Invalid Username/Password", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    Log.d("Testing", "Log in as driver!");
+                    MutableLiveData<Driver> liveRider = DatabaseManager.getInstance().logInAsDriver(
+                            usernameField.getText().toString(), passwordField.getText().toString(), this);
+                    liveRider.observe(this, driver -> {
+                        if (driver != null) {
+                            Log.d("Testing", "Login Success");
+                            Log.d("Testing", "Driver Main Activity not yet in this branch");
+                            Intent intent = new Intent(this, DriverMainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Invalid Username/Password", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+
+            public void onRegisterPressed(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                String username = usernameField.getText().toString();
+                intent.putExtra("USERNAME", username);
+                startActivity(intent);
+            }
+
+
         }
-
-        radioButtonRider = findViewById(R.id.rider_radio_button);
-        buttonLogIn = findViewById(R.id.button_log_in);
-
-        buttonLogIn.setOnClickListener(view -> onClick_signIn());
-        
-        this.usernameField = findViewById(R.id.username_field);
-        this.passwordField = findViewById(R.id.password_field);
-        this.loginTypeField = findViewById(R.id.rider_driver_toggle);
-
-        // Set the selector to select Rider by default
-        this.loginTypeField.check(R.id.rider_radio_button);
-    }
-
-    private void onClick_signIn() {
-        // todo: proper implementation of sign in
-        if (radioButtonRider.isChecked()) {
-            Intent intent = new Intent(this, RiderMainActivity.class);
-            startActivity(intent);
-        } else {
-            Intent intent = new Intent(this, DriverMainActivity.class);
-            startActivity(intent);
-            /**
-            Log.d("Testing", "onClick_signIn: Driver");
-//            DatabaseManager.getInstance().registerRider("Appletun",
-//                    "yum yum jelly jelly",
-//                    "mrmr@gmail.com",
-//                    "Appletun",
-//                    "3.14",
-//                    "123",
-//                    this);
-            DatabaseManager.getInstance().logInAsDriver(
-                    "Thomas", "choo choo", this)
-                    .observe(this, driver -> Log.d("Testing", "onChanged: Success: " + (driver != null)));
-             **/
-        }
-
-    }
-
-    public void onLoginPressed(View view) {
-        // Check for empty fields
-        if(usernameField.getText().toString().isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Username Required", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if(passwordField.getText().toString().isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Password Required", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        // Todo: Send login details to database for login validation
-        // if the email doesn't exist, should we transition to register screen automatically?
-        // Todo: Transition to appropriate screen (Rider/Driver)
-    }
-
-    public void onRegisterPressed(View view) {
-        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-        String username = usernameField.getText().toString();
-        intent.putExtra("USERNAME", username);
-        startActivity(intent);
-    }
-
-
-}
