730d298860f5d983c0139fbc53a4375009aad6d1
Allan Manuba <manuba@ualberta.ca>
2020-03-29 14:20:04 -0600

    Prepare UserDAO testing

---
+++ app/src/androidTest/java/com/cmput301w20t10/uberapp/UserDAOTests.java
@@ -0,0 +1,20 @@
+package com.cmput301w20t10.uberapp;
+
+import com.cmput301w20t10.uberapp.util.DatabaseTestBase;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class UserDAOTests extends DatabaseTestBase {
+    private static final String TAG = "Tomate";
+
+    @Before
+    public void initialize() {
+        super.initialize();
+    }
+
+    @After
+    public void cleanUp() {
+        super.cleanUp();
+    }
+}
