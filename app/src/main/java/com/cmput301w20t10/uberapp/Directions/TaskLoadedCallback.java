6cfc6fc9de80860b2439aa31f455ae8725f7a079
Titaniayuki <57152556+Titaniayuki@users.noreply.github.com>
2020-04-01 15:42:21 -0600

    comments

--- app/src/main/java/com/cmput301w20t10/uberapp/Directions/TaskLoadedCallback.java
+++ app/src/main/java/com/cmput301w20t10/uberapp/Directions/TaskLoadedCallback.java
@@ -1,5 +1,11 @@
 package com.cmput301w20t10.uberapp.Directions;
-
+/*
+ * TaskLoadedCallback is code from GITHUB
+ * URL of master: https://github.com/Vysh01/android-maps-directions
+ * Author: Vysh01, https://github.com/Vysh01
+ * Website: https://github.com/
+ * URL of class: https://github.com/Vysh01/android-maps-directions/blob/master/app/src/main/java/com/thecodecity/mapsdirection/directionhelpers/TaskLoadedCallback.java
+ */
 public interface TaskLoadedCallback {
     void onTaskDone(Object... values);
 }
